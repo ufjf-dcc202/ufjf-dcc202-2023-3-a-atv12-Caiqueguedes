8c8dcb1e65659083c616172cc5bd61b49885076d
Caique Guedes <caique.guedes@estudante.ufjf.br>
2023-12-06 19:53:59 -0300

    corrige ortografia

--- estoque.js
+++ estoque.js
@@ -1,11 +1,11 @@
 //estoque.js
-let estoque ={
-joao = [
-    {tipo:"maca", qtd: 1},
-    {tipo:"pera", qtd: 1}
-    ],
- maria = [
-    {tipo:"maca", qtd: 2},
-    {tipo: "banana", qtd: 4}
-    ],
-}
+let estoque = {
+  joao: [
+    { tipo: "maca", qtd: 1 },
+    { tipo: "pera", qtd: 1 },
+  ],
+  maria: [
+    { tipo: "maca", qtd: 2 },
+    { tipo: "banana", qtd: 4 },
+  ],
+};
