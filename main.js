6b736cb9208de88157094e1335b55585f501f404
Caique Guedes <caique.guedes@estudante.ufjf.br>
2023-12-13 20:08:48 -0300

    melhora codigo

--- main.js
+++ main.js
@@ -1,18 +1,44 @@
-//main.js
-console.log(document.entrada);
-document.entrada.addEventListener("submit", leFormulario);
+import { getEstoque, transacao } from "./estoque.js";
+
+document.entrada.addEventListener("submit", leFormulario());
 //leFormulario();
 
 const olJoao = document.querySelector("#olJoao");
 const olMaria = document.querySelector("#olMaria");
 
+atualizaTela();
+
 function leFormulario(event) {
   event.preventDefault();
   const fruta = document.entrada.fruta.value;
-  const quantidade = document.entrada.quantidade.value;
+  const quantidade = document.entrada.quantidade.valueAsNumber;
   const origem = document.entrada.origem.value;
   const destino = document.entrada.destino.value;
 
   console.log(`Solicitado: ${origem} doa ${quantidade} ${fruta} para ${destino}`);
   //document.entrada.submit();
+
+  transacao(origem, destino, quantidade, fruta);
+  atualizaTela();
+}
+
+function atualizaTela() {
+  const estoque = getEstoque();
+  const joao = estoque['joao'];
+  olJoao.innerHTML = "";
+  for(let i=0; i < joao.length; i++){
+    const monte = joao[i];
+    const eLi = document.createElement('li');
+    eLi.innerText = '${monte.fruta}: ${monte.qtd}';
+    olJoao.append(eLi);
+  }
+
+  const maria = estoque['maria'];
+  olJoao.innerHTML = "";
+  for(let i=0; i < maria.length; i++){
+    const monte = maria[i];
+    const eLi = document.createElement('li');
+    eLi.innerText = '${monte.fruta}: ${monte.qtd}';
+    olMaria.append(eLi);
+  }
 }
