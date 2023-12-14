5200da2288cbd510c585f691e03c7dbabf7d506f
Caique Guedes <caique.guedes@estudante.ufjf.br>
2023-12-14 14:31:26 -0300

    incrementa funcoes passsadas em aula

--- main.js
+++ main.js
@@ -1,40 +1,62 @@
-import { getEstoque, transacao } from "./estoque.js";
 
-document.entrada.addEventListener("submit", leFormulario);
-//leFormulario();
+import { getEstoque, limpaEstoque, transacaoNoEstoque } from "./estoque.js";
 
-const olJoao = document.querySelector("#olJoao");
-const olMaria = document.querySelector("#olMaria");
+document.entrada.addEventListener('submit', leFormulario);
+
+
+const olJoao = document.querySelector("#joao");
+const olMaria = document.querySelector("#maria");
 
 atualizaTela();
 
-function leFormulario(event) {
-  event.preventDefault();
-  const fruta = document.entrada.fruta.value;
-  const quantidade = document.entrada.quantidade.valueAsNumber;
-  const origem = document.entrada.origem.value;
-  const destino = document.entrada.destino.value;
+function leFormulario(event)
+{
+    event.preventDefault();
+    
+    const fruta = document.entrada.fruta.value;
+    const quantidade = document.entrada.quantidade.valueAsNumber;
+    const origem = document.entrada.origem.value;
+    const destino = document.entrada.destino.value;
+    
+    console.log(`solicitado: ${origem} doa ${quantidade} ${fruta} para ${destino}`);
 
-  console.log(`Solicitado: ${origem} doa ${quantidade} ${fruta} para ${destino}`);
-  //document.entrada.submit();
 
-  transacao(origem, destino, quantidade, fruta);
-  atualizaTela();
+    transacaoNoEstoque(origem, destino, fruta, quantidade);
+    atualizaTela();
 }
 
-function atualizaTela() {
-  const estoque = getEstoque();
-  
-  preencheListaPessoa(estoque['joao'], olJoao);
-  preencheListaPessoa(estoque['maria'], olMaria);
+document.addEventListener('DOMContentLoaded', () => 
+{
+    document.getElementById('botaoLimparLista').addEventListener('click', () => 
+    {
+        limpaEstoque();
+        atualizaTela();
+    });
+});
+
+function atualizaTela() 
+{
+    const estoque = getEstoque();
+    
+    document.entrada.fruta.value = "maca";
+    document.entrada.quantidade.value = 1;
+   
+    preencheListaPessoa(estoque['joao'], olJoao);
+    preencheListaPessoa(estoque['maria'], olMaria);
 }
 
-function preencheListaPessoa(pessoa, lista){
-  lista.innerHTML = "";
-  for(let i=0; i < pessoa.length; i++){
-    const monte = pessoa[i];
-    const eLi = document.createElement('li');
-    eLi.innerText = '${monte.tipo}: ${monte.qtd}';
-    olMaria.append(eLi);
-  }
+function preencheListaPessoa(pessoa, lista)
+{
+    lista.innerHTML = "";
+
+    if(Array.isArray(pessoa)) 
+    {
+        for(let i = 0; i < pessoa.length; i++) 
+        {
+            const monte = pessoa[i];
+            const eLi = document.createElement('li');
+            eLi.innerText = `${monte.tipo}: ${monte.quantidade}`;
+            lista.append(eLi);
+        }
+    }
 }
